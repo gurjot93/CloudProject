ab62a3921ebea1fb639b727f2296f188545c6d43
Mohan Pathania <mohanp@outlook.com>
2019-04-08 18:00:56 -0300

    Added apps in settings

--- websites/settings.py
+++ websites/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'empweb',
+    'mbrweb',
+    'reweb',
 ]
 
 MIDDLEWARE = [
