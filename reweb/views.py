620bfa57d571245fc55a322ad0d6f07abadb07c7
Jonathan Elliott <jn288377@dal.ca>
2019-04-09 11:46:50 -0300

    Can create dummy users in RE Web service

--- reweb/views.py
+++ reweb/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
+from .models import RealEstate
 
 
 # Create your views here.
 def index(request):
-    return render(request,'re_login.html')
+    return render(request,'choose_form.html')
 
 def mortId(request):
     ##Token from the request could be used for Security...maybe???
-    print(request.POST)
-    return render(request,'re_mortId.html')
+     page='re_mortId.html'
+     if request.method == "POST":
+          print(request.POST)
+          try:
+            reaEstateUser =RealEstate.objects.get(mortID=request.POST.get('mortID',''))
+            print(reaEstateUser.name)
+           ##If there is no current user by that mortId then add a new user
+          except Exception:
+            print('No User found;creating a new user')
+            name= request.POST.get('name','')
+            mortID=request.POST.get('mortID','')
+            realEstateUser= RealEstate(
+                name=name,
+                mortID=mortID)
+            print("Saving New User into RealEstate DB")
+            realEstateUser.save()
+            realEstateUsers =RealEstate.objects.all()
+            print(request.POST)
+            print(realEstateUsers)
+     return render(request,page)
