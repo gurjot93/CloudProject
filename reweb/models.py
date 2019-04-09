d72db180627a72cbb4d3b734d2c9ad2659288f5e
Jonathan Elliott <jn288377@dal.ca>
2019-04-09 11:02:08 -0300

    Made changes To REweb model...tweak RE web service workflow

--- reweb/models.py
+++ reweb/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class RealEstate(models.Model):
-    username = models.CharField(max_length=100,unique=True)
-    password = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    mortID = models.CharField(max_length=100,unique=True)
 
 def __str__(self):
     return self.firstName
